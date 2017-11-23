ea8d62c2eae1760fee61452b4cf1130e47c4e491
Elijah <elijahsharper1@gmail.com>
2017-11-24 01:31:59 +0300

    FrequenciesRebuilder

--- app/Scheduler/Frequencies.php
+++ app/Scheduler/Frequencies.php
@@ -7,20 +7,36 @@ trait Frequencies
     public function cron($expression)
     {
         $this->expression = $expression;
+
+        return $this;
     }
 
     public function everyMinute()
     {
-      $this->expression = '* * * * *';
+      // default
+      return $this->cron($this->expression);
     }
 
     public function everyTenMinutes()
     {
-      $this->expression = '*/10 * * * *';
+      return $this->replaceIntoExpression(1,'*/10');
     }
 
     public function everyThirtyMinutes()
     {
-      $this->expression = '*/30 * * * *';
+      return $this->replaceIntoExpression(1,'*/30');
+    }
+
+    public function replaceIntoExpression($postion, $value)
+    {
+        $value = (array) $value;
+
+        $expression = explode(' ', $this->expression);
+
+        array_splice($expression, $postion - 1, 1, $value);
+
+        $expression = array_slice($expression, 0, 5);
+
+        return $this->cron(implode(' ', $expression));
     }
 }
--- tests/Unit/FrequenciesTest.php
+++ tests/Unit/FrequenciesTest.php
@@ -7,6 +7,49 @@ use PHPUnit\Framework\TestCase;
 
 class FrequenciesTest extends TestCase
 {
+
+    /** @test */
+    public function can_replace_into_expression_at_position()
+    {
+        $frequencies = $this->frequencies();
+
+        $frequencies->replaceIntoExpression(1,1);
+
+        $this->assertEquals($frequencies->expression, '1 * * * *');
+    }
+
+    /** @test */
+    public function can_replace_into_expression_by_chaining()
+    {
+        $frequencies = $this->frequencies();
+
+        $frequencies->replaceIntoExpression(1,1)->replaceIntoExpression(2,2);
+
+        $this->assertEquals($frequencies->expression, '1 2 * * *');
+    }
+
+
+    /** @test */
+    public function can_replace_into_expression_with_array()
+    {
+        $frequencies = $this->frequencies();
+
+        $frequencies->replaceIntoExpression(1,[1,2]);
+
+        $this->assertEquals($frequencies->expression, '1 2 * * *');
+    }
+
+    /** @test */
+    public function can_not_replace_past_the_end_of_an_expression()
+    {
+        $frequencies = $this->frequencies();
+
+        $frequencies->replaceIntoExpression(5,[1,2]);
+
+        $this->assertEquals($frequencies->expression, '* * * * 1');
+    }
+
+
     /** @test */
     public function can_set_plain_cron_expression()
     {
